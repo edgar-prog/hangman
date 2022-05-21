aee846a9569b28c7614fc498318a7c5879215487
edgar <sy3dg4rm4rt1n3z@hotmail.com>
2022-05-20 23:37:12 -0600

    ya esta el diseño, se comienza a programar la logica de funcionamiento

--- js/game.js
+++ js/game.js
@@ -8,9 +8,20 @@ buitre.src = "img/buitre.png";
 var hangman = new Image();
 hangman.src = "img/hangman.png";
 
+var arbol = new Image();
+arbol.src = "img/arbol.png";
+
+var soga = new Image();
+soga.src = "img/soga.png";
+
+var spech = new Image();
+spech.src = "img/spech.png";
+
 var step1 = 0;
 function dibujarBuitre() {
 	ctx.clearRect(550,150,81,129);
+	ctx.drawImage(arbol, 0, 0, 418, 436, 90, 115, 376, 392);
+	ctx.drawImage(spech, 0, 0, 400, 300, 385, 10, 280, 210);
 	ctx.drawImage(buitre, (step1++ % 3) * 108, 0, 108, 172, 550, 150, 81, 129); 
 	if(step1 > 3) {
 		step1 = 0;
@@ -19,14 +30,13 @@ function dibujarBuitre() {
 
 var step2 = 0;
 function dibujarHangman() {
-	ctx.clearRect(300,225,144,240); 
-	ctx.drawImage(hangman, (step2++ % 14) * 48, 0, 48, 80, 300, 225, 144, 240); 
-	if(step2 > 14) {
+	ctx.clearRect(250,250,192,192); 
+	ctx.drawImage(soga, 0, 0, 92, 136, 315, 200, 83, 122);
+	ctx.drawImage(hangman, (step2++ % 7) * 270, 0, 270, 270, 250, 250, 192, 192); 
+	if(step2 > 7) {
 		step2 = 0;
 	}
 }
 
-
-
 setInterval(dibujarBuitre, 1000/3);
-setInterval(dibujarHangman, 6000/14);
+setInterval(dibujarHangman, 6000/7); 
--- js/teclado.js
+++ js/teclado.js
@@ -15,3 +15,16 @@ for (var i = 0; i < alfabeto.length; i++) {
 	letras.appendChild(lista);
 }
  
+ 
+var teclasPresionadas = [];
+var palabra = "manzana";
+
+var teclado = document.getElementById("alfabeto");
+
+teclado.addEventListener("click",function(event) {
+	tecla = event.target.textContent;
+	teclasPresionadas += tecla;
+	if(palabra.includes(tecla)) {
+		console.log("letra"+teclasPresionadas);
+	}
+});
