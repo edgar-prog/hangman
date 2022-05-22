2365bc5ccc80120b9c635fb346070b37bd5b2a21
edgar <sy3dg4rm4rt1n3z@hotmail.com>
2022-05-21 23:13:57 -0600

    se realizo la funcion de llenado de palabra

--- js/teclado.js
+++ js/teclado.js
@@ -2,8 +2,22 @@
 var alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
-		
-var div = document.querySelector(".div-teclado");
+
+var palabra = "transmetro";
+
+var div_visor = document.querySelector(".div-visor");
+var adivina = document.createElement("ul");
+
+for (var i = 0; i < palabra.length; i++) {
+	adivina.id = "espacios";
+	espacio = document.createElement("li");
+	/* espacio.id = "letra"; */
+	espacio.innerHTML = "_";
+	div_visor.appendChild(adivina);
+	adivina.appendChild(espacio);
+}
+
+var div_teclas = document.querySelector(".div-teclas");
 var letras = document.createElement("ul");
 
 for (var i = 0; i < alfabeto.length; i++) {
@@ -11,20 +25,33 @@ for (var i = 0; i < alfabeto.length; i++) {
 	lista = document.createElement("li");
 	lista.id = "letra";
 	lista.innerHTML = "<b>"+alfabeto[i]+"</b>";
-	div.appendChild(letras);
+	div_teclas.appendChild(letras);
 	letras.appendChild(lista);
 }
  
- 
-var teclasPresionadas = [];
-var palabra = "manzana";
-
 var teclado = document.getElementById("alfabeto");
+var espaciosVacios = document.getElementById("espacios");
 
 teclado.addEventListener("click",function(event) {
 	tecla = event.target.textContent;
-	teclasPresionadas += tecla;
 	if(palabra.includes(tecla)) {
-		console.log("letra"+teclasPresionadas);
+		var posicionesLetras = localizarLetras(palabra, tecla);
+		
+		for(var j = 0; j < posicionesLetras.length; j++) {
+			var elemento = document.createElement("li")
+			elemento.innerHTML = tecla;
+			var posicion = posicionesLetras[j];
+			espaciosVacios.replaceChild(elemento,espaciosVacios.children[posicion]);
+		}
 	}
 });
+
+function localizarLetras(palabra, letra) {
+	var posiciones = [];
+	for(i = 0; i < palabra.length; i++) {
+		if(palabra[i] == letra) {
+			posiciones.push(i);
+		}
+	}
+	return posiciones;
+}
