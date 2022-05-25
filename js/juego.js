7b8e7bb98fa45f0dc0ac87b3e6f4499149935130
edgar <sy3dg4rm4rt1n3z@hotmail.com>
2022-05-25 15:33:27 -0600

    arreglo la visualizacion de las palabras ya ingresadas

--- js/juego.js
+++ js/juego.js
@@ -1,5 +1,5 @@
 
-var palabrasAdivinar = ["MANZANA","PERFUME","ARBOL","HIELO","ROBOT","ZAPATO","EDIFICIO"];
+
 
 function generarPalabra() {
 	var indice = Math.floor(Math.random() * 7);
--- js/save.js
+++ js/save.js
@@ -1,2 +1,21 @@
 
-let listaPalabras = document.getElementById("lista-palabras");
+var palabrasAdivinar = ["MANZANA","PERFUME","ARBOL","HIELO","ROBOT","ZAPATO","EDIFICIO","MESA","SILLA","CEBOLLA","OELDA"];
+
+var listaPalabras = document.getElementById("lista-palabras");
+	
+for (var k = 0; k < palabrasAdivinar.length; k++) {
+	nuevoItem = document.createElement("li");
+	nuevoItem.innerHTML = palabrasAdivinar[k];
+	listaPalabras.appendChild(nuevoItem);
+}	
+
+
+var botonAdicionar = document.getElementById("agregar-palabra");
+
+botonAdicionar.addEventListener("click",function(event){
+	event.preventDefault();
+	var nuevaPalabra = document.getElementById("palabra").value;
+	nuevoItem = document.createElement("li");
+	nuevoItem.innerHTML = nuevaPalabra;
+	listaPalabras.appendChild(nuevoItem);
+});
