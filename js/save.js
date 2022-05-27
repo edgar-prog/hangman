799eff559e4b8a4428a86df4dd672079e41c06c4
edgar <sy3dg4rm4rt1n3z@hotmail.com>
2022-05-27 15:49:38 -0600

    terminada la pagina donde guardan las palabras

--- js/save.js
+++ js/save.js
@@ -9,14 +9,34 @@ for (var k = 0; k < palabrasAdivinar.length; k++) {
 	listaPalabras.appendChild(nuevoItem);
 }	
 
-
 var botonAdicionar = document.querySelector(".btn-add");
 
 botonAdicionar.addEventListener("click",function(event){
 	event.preventDefault();
 	var nuevaPalabra = document.getElementById("palabra");
-	nuevoItem = document.createElement("li");
-	nuevoItem.innerHTML = nuevaPalabra.value;
-	listaPalabras.appendChild(nuevoItem);
 	
+	if ((nuevaPalabra.value.length) > 0 && (verificarTexto(nuevaPalabra.value))) {
+		if(palabrasAdivinar.length < 20) {
+			nuevoItem = document.createElement("li");
+			nuevoItem.innerHTML = nuevaPalabra.value.toUpperCase();
+			listaPalabras.appendChild(nuevoItem);
+			palabrasAdivinar.push(nuevaPalabra.value.toUpperCase());
+		}
+		else {
+			alert("20 palabras maximo");
+		}
+	}
+	else {
+		alert("palabra no valida...  intente de nuevo");
+	}
+
+	document.getElementById("palabra").value = "";
 });
+
+function verificarTexto(verificar) {
+	var regex = /^[a-zA-Z]+$/;
+	if(!regex.test(verificar))
+		return false
+	else
+		return true;
+}
